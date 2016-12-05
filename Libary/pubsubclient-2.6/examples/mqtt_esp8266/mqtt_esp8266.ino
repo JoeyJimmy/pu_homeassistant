f13098ff2c01be4c5c9b54741f91f9ad6b0cdbe2
chasin <aaiiaann2000@gmail.com>
2016-12-05 21:21:31 +0800

    Ardunio Libary
    這是需要的Libary

--- Libary/pubsubclient-2.6/examples/mqtt_esp8266/mqtt_esp8266.ino
+++ Libary/pubsubclient-2.6/examples/mqtt_esp8266/mqtt_esp8266.ino
@@ -28,16 +28,23 @@
 
 // Update these with values suitable for your network.
 
-const char* ssid = "cmrdb_wlan";
-const char* password = "cmrdb_base@402";
-
-const char* mqtt_server = "192.168.43.8";
+const char* ssid = "........";
+const char* password = "........";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
 
 void setup_wifi() {
 
@@ -59,10 +66,11 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
+
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
-  Serial.print("]: ");
+  Serial.print("] ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
@@ -78,29 +86,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 
 }
-void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 10449); // need to change to your port number
-  client.setCallback(callback);
-}
-
-
-
-
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client","homeassistant","")) {
+    if (client.connect("ESP8266Client")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("hello", "hello world");
+      client.publish("outTopic", "hello world");
       // ... and resubscribe
-      client.subscribe("hello");
+      client.subscribe("inTopic");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -118,12 +115,12 @@ void loop() {
   client.loop();
 
   long now = millis();
-  if (now - lastMsg > 5000) {
+  if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-    snprintf (msg, 50, "hello world #%ld", value);
+    snprintf (msg, 75, "hello world #%ld", value);
     Serial.print("Publish message: ");
     Serial.println(msg);
-    client.publish("hello", msg);
+    client.publish("outTopic", msg);
   }
 }
